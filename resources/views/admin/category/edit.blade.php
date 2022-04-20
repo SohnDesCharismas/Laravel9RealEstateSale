a7020edf0da84c220b333198db8a75a4e96c04ef
SohnDesCharismas <mor-bamya@hotmail.com>
2022-04-20 06:09:02 +0300

    Category update to database (Laravel 9 Video.12th)

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.adminbase')
 
-@section('title', 'Add Category')
+@section('title', 'Edit Category :'.$data->title)
 
 
 
@@ -8,12 +8,12 @@
 
     <div id="page-wrapper">
         <div class="col-sm-6">
-            <h1>Add Category</h1>
+            <h1>Edit Category : {{$data->title}}</h1>
         </div>
 
             <ol class="breadcrumb float-right">
                 <li class="breadcrumb-item"><a href="/admin">Home</a></li>
-                <li class="breadcrumb-item active">Add Category</li>
+                <li class="breadcrumb-item active">Edit Category</li>
             </ol>
 
         <div class="col-md-12 col-sm-12 col-xs-12">
@@ -22,19 +22,19 @@
                 <div class="card-header">
                    <h3 class="card-title">Category Elements</h3>
                 </div>
-                <form role="form" action="/admin/category/store" method="post">
+                <form role="form" action="/admin/category/update/{{$data->id}}" method="post">
                         @csrf
                         <div class="form-group">
                             <label>Title</label>
-                            <input class="form-control" type="text" placeholder="Title" name="title">
+                            <input class="form-control" type="text" value="{{$data->title}}" name="title">
                         </div>
                         <div class="form-group">
                             <label>Keywords</label>
-                            <input class="form-control" type="text" placeholder="Keywords" name="keywords">
+                            <input class="form-control" type="text" value="{{$data->keywords}}" name="keywords">
                         </div>
                         <div class="form-group">
                             <label>Description</label>
-                            <input class="form-control" type="text" placeholder="Description" name="description">
+                            <input class="form-control" type="text" value="{{$data->description}}" name="description">
                         </div>
                         <div class="form-group">
                             <label for="exampleInputFile">Image</label>
@@ -51,12 +51,13 @@
                          <div class="form-group">
                                    <label>Status</label>
                                    <select class="form-control" name="status">
-                                   <option>True</option>
-                                   <option>False</option>
+                                       <option selected>{{$data->status}}</option>
+                                       <option>True</option>
+                                       <option>False</option>
                                    </select>
                         </div>
                         <div class="card-footer">
-                            <button class="btn btn-primary" type="submit">Save</button>
+                            <button class="btn btn-primary" type="submit">Update data</button>
                         </div>
                     </form>
             </div>
